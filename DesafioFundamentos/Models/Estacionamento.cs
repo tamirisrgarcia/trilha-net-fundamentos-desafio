0e8069f374b2298b2f2b1e4b550553d3f3b3e8c9
TAMIRIS REZENDE GARCIA COSTA <tamirisrgarcia@gmail.com>
2024-01-08 20:33:36 -0300

    desafio concluido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -29,6 +29,7 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
                 int horas = Convert.ToInt32(Console.ReadLine());
                 decimal valorTotal = precoInicial + precoPorHora * horas; 
+                
                 veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -45,7 +46,7 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 foreach (string placa in veiculos)
                 {
-                    Console.WriteLine($"{placa}");
+                    Console.WriteLine(placa);
                 }
             }
             else
